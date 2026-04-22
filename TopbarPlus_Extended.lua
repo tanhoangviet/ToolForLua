eccdc73c8455382763f6830666caada0d65c9713
AmongUs <128269328+tanhoangviet@users.noreply.github.com>
2026-04-22 21:46:29 +0700

    Update TopbarPlus_Extended.lua

--- TopbarPlus_Extended.lua
+++ TopbarPlus_Extended.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tanhoangviet/ToolForLua/refs/heads/main/TopbarPlus.lua"))()
+local _bundle = loadstring(game:HttpGet("https://raw.githubusercontent.com/tanhoangviet/ToolForLua/refs/heads/main/TopbarPlus.lua"))()
 local Icon    = _bundle.get()
 assert(Icon, "[TopbarPlusEx] Failed to load TopbarPlus bundle!")
 
